abfab0fba7bba3de9005bd8a219c61173c0c83ca
salino3 <flaviobcn012@gmail.com>
2023-03-10 12:28:09 +0100

    commit 2

--- src/connection/db.js
+++ src/connection/db.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: `${process.env.USER}`,
   password: `${process.env.PASSWORD}`,
-  database: "repasando",
+  database: "",
   port: `${process.env.PORTDB}`,
 });
 
